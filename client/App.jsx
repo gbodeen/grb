d4e8e70b930011df52fc420ddc8151c53aa30518
Gabriel Bodeen <gabebodeen@gmail.com>
2019-03-01 16:43:19 -0600

    In progress: Skewed box jiggles; adjusting relative units now.

--- client/App.jsx
+++ client/App.jsx
@@ -8,10 +8,10 @@ const App = () => {
     <>
       <div className="splashpage">
         <div className="splashbox">
-          <div className="splashtext">
-            <h1>Welcome</h1>
-            <p>Inclusive, shared vocabulary; thought leadership inclusion, and cultivate theory of change disrupt academic. Gender rights, systems thinking co-create, shared unit of analysis engaging paradigm humanitarian. Changemaker program area radical compelling LGBTQ+ framework innovate. State of play, resist academic; shine policymaker catalyze. Low-hanging fruit greenwashing, radical emerging indicators data the resistance effective altruism boots on the ground. Strengthening infrastructure vibrant outcomes compassion citizen-centered shine, incubator scalable. Game-changer; justice impact radical boots on the ground philanthropy issue outcomes. </p>
-          </div>
+        </div>
+        <div className="splashtext">
+          <h1>Welcome</h1>
+          <p>Inclusive, shared vocabulary; thought leadership inclusion, and cultivate theory of change disrupt academic. Gender rights, systems thinking co-create, shared unit of analysis engaging paradigm humanitarian. Changemaker program area radical compelling LGBTQ+ framework innovate. State of play, resist academic; shine policymaker catalyze. Low-hanging fruit greenwashing, radical emerging indicators data the resistance effective altruism boots on the ground. Strengthening infrastructure vibrant outcomes compassion citizen-centered shine, incubator scalable. Game-changer; justice impact radical boots on the ground philanthropy issue outcomes. </p>
         </div>
         <nav><span>link 1</span><span>link 2</span><span>link 3</span><span>link 4</span></nav>
       </div>
