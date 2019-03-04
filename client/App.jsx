69ee76bc6898a32e1378828f03111b362567fdd9
Gabriel Bodeen <gabebodeen@gmail.com>
2019-03-04 17:52:51 -0600

    In progress: Switching plain CSS to SCSS.

--- client/App.jsx
+++ client/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Bubbles from './Bubbles';
 import Footer from './Footer';
+import './styles.scss';
 
 const App = () => {
   return (
